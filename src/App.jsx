9a13bc54a6c9cd5a65ca88a9bc68a8eab6f7c805
Venisha <149953166+getstar1@users.noreply.github.com>
2025-08-19 12:28:24 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   const handleChange = (e) => {
     setProduct({ ...product, [e.target.name]: e.target.value });
   };
-const BASE_URL = 'http://localhost:8095/springapp1/';
+const BASE_URL = 'http://localhost:9099'';
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
